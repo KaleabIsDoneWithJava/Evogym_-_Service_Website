5da80249737fdf963e02cdf9a512a65b5a54a9e1
KaleabIsDoneWithJava <kaleab.teshome16@gmail.com>
2023-12-24 08:59:34 +0300

    commented useMediaQuery

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -1,19 +1,32 @@
 import { useState, useEffect } from "react";
-
+// Create a custom hook named useMediaQuery that takes a media query string as input
 const useMediaQuery = (query: string) => {
+  // State to store whether the media query matches
   const [matches, setMatches] = useState(false);
 
+  // Effect to handle media query matching and updates
   useEffect(() => {
+    // Get a MediaQueryList object representing the specified query
     const media = window.matchMedia(query);
+
+    // Update the matches state if the current state differs from the query's matches
     if (media.matches !== matches) {
       setMatches(media.matches);
     }
-    const listener = () => setMatches(media.matches);
-    window.addEventListener("resize", listener);
-    return () => window.removeEventListener("resize", listener);
-  }, [matches, query]);
 
+    // Listener function to update matches state on window resize
+    const listener = () => setMatches(media.matches);
+
+    // Add event listener for window resize
+    window.addEventListener("resize", listener);
+
+    // Cleanup function to remove event listener on component unmount
+    return () => window.removeEventListener("resize", listener);
+  }, [matches, query]); // Only re-run the effect if matches or query change
+
+  // Return the matches state to indicate whether the query matches
   return matches;
 };
 
+// Export the hook for use in other components
 export default useMediaQuery;
