47c0aa5f8ebeb288496b4da952a1f78621960817
KaleabIsDoneWithJava <kaleab.teshome16@gmail.com>
2024-02-08 13:00:30 +0300

    Added Header animation for Benefits

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -25,7 +25,7 @@ const benefits: Array<BenefitType> = [
     icon: <AcademicCapIcon className="h-6 w-6" />,
     title: "Expert and Pro Trainers",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed et dolore magna aliqua. ",
   },
 ];
 
@@ -46,14 +46,25 @@ const Benefits = ({ setSelectedPage }: Props) => {
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)} // To make the navbar highlight the page name when you scroll back from bottom.
       >
-        <div className="md:my-5 md:w-3/5">
+        {/* HEADER */}
+        <motion.div
+          className="md:my-5 md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
           <HText>MORE THAN JUST A GYM.</HText>
           <p className="my-5 text-sm">
             We provide world class fitness equipment, trainers, and classes to
             get you to your ultimate fitness goals with ease. We provide true
             care into each and every member.
           </p>
-        </div>
+        </motion.div>
       </motion.div>
 
       {/* BENEFITS */}
