f57aa1b1ed39e1ed7b9c29ec4dbe7b17dd3ec3ab
KaleabIsDoneWithJava <kaleab.teshome16@gmail.com>
2024-02-16 09:58:18 +0300

    Added a color in tailwind config

--- src/scenes/contactus/index.tsx
+++ src/scenes/contactus/index.tsx
@@ -9,6 +9,8 @@ type Props = {
 };
 
 function ContactUs({ setSelectedPage }: Props) {
+  const inputStyles = `"w-full rounded-lg border-2 border-gray-500 px-5 py-3 placeholder-gray-500"`;
+
   const {
     register,
     trigger, //allows us to validate our form if needed. Async function.
@@ -62,10 +64,28 @@ function ContactUs({ setSelectedPage }: Props) {
               visible: { opacity: 1, y: 0 },
             }}
           >
-            <form target="blank" onSubmit={onSubmit} action="https://formsubmit.co/el/hobime♠" method="POST">
+            <form
+              target="blank"
+              onSubmit={onSubmit}
+              action="https://formsubmit.co/el/hobime"
+              method="POST"
+            >
               <input
-
-               />
+                className="w-full rounded-lg border-2 border-gray-300 px-5 py-3 placeholder-gray-500 active:border-gray-500"
+                type="text"
+                placeholder="NAME"
+                {...register("name", {
+                  required: true,
+                  maxLength: 100,
+                })}
+              />
+              {errors.name && (
+                <p className="mt-1 text-red-500 ">
+                  {errors.name.type === "required" && "This Field is required."}
+                  {errors.name.type === "maxLength" &&
+                    "Max length is 100 characters."}
+                </p>
+              )}
             </form>
           </motion.div>
         </div>
