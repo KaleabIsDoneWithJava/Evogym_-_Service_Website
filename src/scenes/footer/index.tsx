5810f8529c46209186fe16bac69320075b48cb5d
KaleabIsDoneWithJava <kaleab.teshome16@gmail.com>
2024-02-18 12:24:49 +0300

    Started footer

---
+++ src/scenes/footer/index.tsx
@@ -0,0 +1,15 @@
+import Logo from "@/assets/Logo.png";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
